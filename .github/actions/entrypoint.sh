b135fff94bbfaae17fa5a501bf60115fa37f1013
Igor Spasić <igor.spasic@gmail.com>
2020-03-26 22:54:06 +0100

    workflow

---
+++ .github/actions/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+
+echo "## Initializing git repo..."
+git init
+echo "### Adding git remote..."
+git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+echo "### Getting branch"
+BRANCH=${GITHUB_REF#*refs/heads/}
+echo "### git fetch $BRANCH ..."
+git fetch origin $BRANCH
+echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
+git checkout $BRANCH
+
+echo "## Login into git..."
+git config --global user.email "igor.spasic@gmail.com"
+git config --global user.name "RepoUpdater"
+
+echo "## Ignore workflow files (we may not touch them)"
+git update-index --assume-unchanged .github/workflows/*
+
+npm run start
+
+echo "## Deleting node_modules..."
+rm -rf node_modules/
+echo "## Staging changes..."
+git add .
+echo "## Commiting files..."
+git commit -m "Repo Updated" || true
+echo "## Pushing to $BRANCH"
+git push -u origin $BRANCH
